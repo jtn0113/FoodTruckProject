5a8c364d0f8bfd3caf7bebf5b9197703ef760e98
John Nichols <jtn0113@yahoo.com>
2022-10-28 19:30:57 -0500

    app functionality complete

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,12 +1,111 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+
+		FoodTruckApp fta = new FoodTruckApp();
+		FoodTruck[] trucks = new FoodTruck[5];
+		int counter = 0;
+
+		while (true) {
+			trucks[counter] = fta.userNewFoodTruck(scanner);
+
+			if (counter == 4 || trucks[counter] == null) {
+				break;
+			}
+			counter++;
+		}
+
+		boolean keepGoing = true;
+		while (keepGoing == true) {
+			switch (fta.showMenu(scanner)) {
+				case 1:
+					fta.showAllTrucks(trucks);
+					break;
+				case 2:
+					System.out.println("\n" + "The average rating of all food trucks is: " + fta.averageRating(trucks) + "\n");
+					break;
+				case 3:
+					System.out.println("\n" + "The highest rated truck is: " + fta.highestRated(trucks) + "\n");
+					break;
+				case 4:
+					keepGoing = false;
+					System.out.println("Dilly Dilly!");
+					break;
+				default:
+					break;
+			}
+		}
+		scanner.close();
 	}
-	// menus
-	// scanner
-	
+
+	public FoodTruck userNewFoodTruck(Scanner scanner) {
+		System.out.println("Enter the food truck name: ");
+		String name = scanner.next();
+		if (name.equals("quit")) {
+			return null;
+		}
+		System.out.println("Enter the food truck food type: ");
+		String foodType = scanner.next();
+		System.out.println("Enter the food truck rating: ");
+		int rating = scanner.nextInt();
+
+		FoodTruck ft = new FoodTruck(name, foodType, rating);
+
+		return ft;
+	}
+
+	public int showMenu(Scanner scanner) {
+		System.out.println("1.) List all existing food trucks.");
+		System.out.println("2.) See the average rating of food trucks.");
+		System.out.println("3.) Display the highest-rated food truck.");
+		System.out.println("4.) Quit the program.");
+
+		return scanner.nextInt();
+	}
+
+	public String averageRating(FoodTruck[] trucks) {
+		int truckTotal = 0;
+		int ratingTotal = 0;
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				ratingTotal += truck.getRating();
+				truckTotal++;
+			}
+		}
+		double average = (double) ratingTotal / truckTotal;
+		return String.format("%.2f", average);
+	}
+
+	public FoodTruck highestRated(FoodTruck[] trucks) {
+		FoodTruck highestRatedTruck = null;
+		int highestRating = 1;
+
+		for (FoodTruck truck : trucks) {
+			if (truck != null && truck.getRating() > highestRating) {
+				highestRating = truck.getRating();
+				highestRatedTruck = truck;
+			}
+		}
+
+		System.out.println(highestRating);
+		return highestRatedTruck;
+	}
+
+	public void showAllTrucks(FoodTruck[] trucks) {
+		System.out.println();
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				System.out.println(truck.toString());
+			}
+		}
+		System.out.println();
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,8 +1,63 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
-	private static int nextTruckId;
+	private static int nextTruckId = 101;
 	private int id;
 	private String name;
+	private String foodType;
+	private int rating;
 	
+	public FoodTruck(String name, String foodType, int rating) {
+		this.id = nextTruckId;
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		nextTruckId ++;
+	}
+
+	public static int getNextTruckId() {
+		return nextTruckId;
+	}
+
+	public static void setNextTruckId(int nextTruckId) {
+		FoodTruck.nextTruckId = nextTruckId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + ", name=" + name + ", foodType=" + foodType + ", rating=" + rating;
+	}
+
 }
